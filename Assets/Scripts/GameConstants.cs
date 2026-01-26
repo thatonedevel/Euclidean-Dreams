597880a1057d9840c5c1aad84ab12fe9d534b8a0
Nora Dwelly <dwelly.412@gmail.com>
2026-01-26 16:49:32 +0000

    Added raycasting constants

--- Assets/Scripts/GameConstants.cs
+++ Assets/Scripts/GameConstants.cs
@@ -1,9 +1,18 @@
+using UnityEngine.Rendering;
+
 namespace GameConstants
 {
     // namespace for important constants & enumerations
+    public static class Constants
+    {
+        public const float MAX_RAYCAST_DISTANCE = 100;
+        public const float MAX_RAYCAST_COUNT = 100;
+    }
 
     namespace Enumerations
     {
+        
+
         public enum Dimensions
         {
             THIRD,
