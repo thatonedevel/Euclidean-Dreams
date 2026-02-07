13c7b55790d754a2f374c809f1005dcfbcd1a03d
Nora Dwelly <dwelly.412@gmail.com>
2026-02-07 15:34:06 +0000

    feat: Added input listening for tutorial stages

--- Assets/Scripts/GUI/TutorialUIController.cs
+++ Assets/Scripts/GUI/TutorialUIController.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
+using GameConstants;
 
 public class TutorialUIController : MonoBehaviour
 {
@@ -11,19 +12,54 @@ public class TutorialUIController : MonoBehaviour
     // inputs we need to listen for
     private InputAction moveAction;
     private InputAction zoomAction;
+    private InputAction cameraLookAction;
     private InputAction perspectiveSwitchAction;
+
+    // events for the actions
+    private PlayerInput.ActionEvent movedEvent;
+    private PlayerInput.ActionEvent zoomEvent;
+    private PlayerInput.ActionEvent lookEvent;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
         // initialise the input
+        moveAction = InputSystem.actions.FindAction(Constants.ACTION_MOVE);
 
+        cameraLookAction = InputSystem.actions.FindAction(Constants.ACTION_ROTATE_CAMERA);
+        zoomAction = InputSystem.actions.FindAction(Constants.ACTION_ZOOM_CAMERA);
+
+        perspectiveSwitchAction = InputSystem.actions.FindAction(Constants.ACTION_SWITCH_PERSPECTIVE);
+
+        // set up the events
+        movedEvent = new PlayerInput.ActionEvent(moveAction);
+        zoomEvent = new PlayerInput.ActionEvent(zoomAction);
+        lookEvent = new PlayerInput.ActionEvent(cameraLookAction);
+
+        // and add the callback
+        movedEvent.AddListener(TutorialActionsListener);
+        zoomEvent.AddListener(TutorialActionsListener);
+        lookEvent.AddListener(TutorialActionsListener);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void TutorialActionsListener(InputAction.CallbackContext context)
     {
-        
+        // just read the action & check which one it is
+        if (context.action == moveAction)
+        {
+            if (tutorialText.lineIndex == 1)
+                tutorialText.AdvanceText();
+        }
+        else if (context.action == zoomAction)
+        {
+             if (tutorialText.lineIndex == 3)
+                tutorialText.AdvanceText();
+        }
+        else if (context.action == cameraLookAction)
+        {
+            if (tutorialText.lineIndex == 2)
+                tutorialText.AdvanceText();
+        }
     }
 }
