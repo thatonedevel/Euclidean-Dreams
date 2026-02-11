b2eccbc8b5fb9848f9ccfa8e84ff590a65788a64
Nora Dwelly <dwelly.412@gmail.com>
2026-02-11 15:30:32 +0000

    Create HUD Controller

---
+++ Assets/Scripts/EGUI/HUDController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using Managers;
+
+namespace EGUI
+{
+   public class HUDController : MonoBehaviour
+   {
+       // Start is called once before the first execution of Update after the MonoBehaviour is created
+       void Start()
+       {
+           
+       }
+   
+       // Update is called once per frame
+       void Update()
+       {
+           
+       }
+   }
+}
