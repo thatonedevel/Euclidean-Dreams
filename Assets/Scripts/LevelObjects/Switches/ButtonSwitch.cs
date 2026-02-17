c052201ea68bee0b7481bde19ed38288c71329d6
Nora Dwelly <dwelly412@gmail.com>
2026-02-17 17:41:03 +0000

    feat: Add button switch

---
+++ Assets/Scripts/LevelObjects/Switches/ButtonSwitch.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System;
+using UnityEngine.Events;
+
+namespace LevelObjects.Switches
+{
+    public class ButtonSwitch : ASwitch
+    {
+        [Header("Button Settings")] [SerializeField]
+        protected float pressCooldown;
+
+        protected float nextPressTime = 0;
+        
+        protected void OnTriggerEnter(Collider other)
+        {
+            // grab the tag of the colliding obj
+            if (Array.IndexOf(validTags, other.tag) != -1)
+            {
+                // check cooldown is elapsed
+                if (Time.time >= nextPressTime)
+                {
+                    // invoke switch enabled event
+                    onSwitchEnabled.Invoke();
+                    nextPressTime = Time.time + pressCooldown;
+                }
+            }
+        }
+    }
+}
