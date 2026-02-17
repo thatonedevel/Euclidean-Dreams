df8db1101fdbf0c77d8fce8bae83c76331dc9acd
Nora Dwelly <dwelly412@gmail.com>
2026-02-17 17:30:24 +0000

    fix: Remove unnecessary invocation method

--- Assets/Scripts/LevelObjects/Switches/ASwitch.cs
+++ Assets/Scripts/LevelObjects/Switches/ASwitch.cs
@@ -6,22 +6,6 @@ namespace LevelObjects.Switches
     public abstract class ASwitch : MonoBehaviour
     {
         [SerializeField] public UnityEvent onSwitchEnabled;
-        
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
-
-        protected void RaiseSwitchEnabledEvent()
-        {
-            onSwitchEnabled.Invoke();
-        }
+        [SerializeField] protected string[] validTags;
     }
 }
