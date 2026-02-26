2a095fdf702f43cfaa44d1eec62f1b3959adc154
Nora Dwelly <dwelly.412@gmail.com>
2026-02-26 17:06:11 +0000

    feat: Create sava data manager

---
+++ Assets/Scripts/Data/Saves/SaveDataManager.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Text;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Serialization;
+using System.IO;
+using System.Threading.Tasks;
+using Managers;
+
+namespace Data.Saves
+{
+    public class SaveDataManager : MonoBehaviour
+    {
+        const string SAVE_NAME = "player_data.json";
+        
+        [Header("Debug Information")]
+        [SerializeField] private SaveSlotData saveData;
+
+        private string saveDataJsonString = String.Empty;
+
+        private Task<string> fileReadTask;
+        
+        // event fired once data is finished writing
+        public static event Action<bool> SaveDataWriteComplete;
+        public static event Action<string> SaveDataReadComplete;
+        
+        // class to manage current save data, including serialisation & i/o operations
+        private void Start()
+        {
+            // check with the game controller how many stages exist
+            int stageCount = GameController.Singleton.TotalLevelCount;
+            
+            saveData = new SaveSlotData(stageCount);
+            
+            // check if the save file exists. if not, create it
+            if (File.Exists(Application.persistentDataPath + "/ " + SAVE_NAME))
+            {
+                // check if the data is valid
+                bool isValid = ReadSaveData();
+                
+                if (!isValid)
+                    WriteSaveData(); // this will override it with a blank object
+            }
+            else
+            {
+                WriteSaveData();
+            }
+        }
+
+        private void WriteSaveData()
+        {
+            // serialize the stored save object to a json string
+            saveDataJsonString = JsonUtility.ToJson(saveData);
+            bool success = true;
+            
+            // use file write as it will be relatively small
+            try
+            {
+                File.WriteAllTextAsync(Application.persistentDataPath + "/ " + SAVE_NAME, saveDataJsonString);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                success = false;
+            }
+            
+            // raise the event to say we're done
+            SaveDataWriteComplete?.Invoke(success);
+        }
+
+        private bool ReadSaveData()
+        {
+            var text = File.ReadAllText(Application.persistentDataPath + "/ " + SAVE_NAME);
+            
+            JsonUtility.FromJson<SaveSlotData>(text);
+        }
+    }
+}
