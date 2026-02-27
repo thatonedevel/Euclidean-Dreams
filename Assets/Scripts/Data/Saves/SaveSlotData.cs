3ca0611417115ae5d34b105a78b9979654a4c49c
Nora Dwelly <dwelly.412@gmail.com>
2026-02-27 12:16:47 +0000

    feat: Add flattened version of gem array for serilisation

--- Assets/Scripts/Data/Saves/SaveSlotData.cs
+++ Assets/Scripts/Data/Saves/SaveSlotData.cs
@@ -15,16 +15,24 @@ namespace Data.Saves
         
         public bool[,] gemCollectionStatus;
 
+        public bool[] gemCollectionStatus_flat;
+
         public SaveSlotData(int levelCount)
         {
             // take the level count in constructor so we can properly size the gem coll status array
             gemCollectionStatus = new bool[levelCount, 3];
+            gemCollectionStatus_flat = new bool[levelCount * 3];
+
+            int iter = 0;
             
             for (int i = 0; i < gemCollectionStatus.GetLength(0); i++)
             {
                 for (int j = 0; j < gemCollectionStatus.GetLength(1); j++)
                 {
                     gemCollectionStatus[i, j] = false;
+                    gemCollectionStatus_flat[iter] = false;
+                    if (iter + 1 < gemCollectionStatus_flat.Length)
+                        iter++;
                 }
             }
         }
