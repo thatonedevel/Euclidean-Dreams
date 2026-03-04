0fb8b852f975cdd16ff70623fff58ba56e33088f
Nora Dwelly <dwelly.412@gmail.com>
2026-03-04 16:41:56 +0000

    feat: Minor tweaks

--- Assets/Scripts/Util.cs
+++ Assets/Scripts/Util.cs
@@ -4,16 +4,16 @@ using GameConstants.Enumerations;
 
 namespace EDreams
 {
+    ///<summary>
+         /// Utility class to help with important game functions
+         ///</summary>
     public static class Util
     {
-        ///<summary>
-        /// Utility class to help with important game functions
-        ///</summary>
         
+        /// <summary>Converts the supplied <para>target</para> Vector3 to a Vector3Int</summary>
         public static Vector3Int ConvertVec3ToInt(Vector3 target)
         {
-            ///<summary>Converts the supplied <para>target</para> Vector3 to a Vector3Int
-            ///</summary>
+            
 
             int x, y, z = 0;
 
