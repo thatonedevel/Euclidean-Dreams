290e944740f3d7af0dfec29caf3e68073f31c763
Nora Dwelly <dwelly.412@gmail.com>
2026-03-16 16:39:58 +0000

    (BROKEN) fix: Player character now moves correctly on the slope face with broken rotation

--- Assets/Scripts/LevelObjects/ForceManipulators/AGravityManipulator.cs
+++ Assets/Scripts/LevelObjects/ForceManipulators/AGravityManipulator.cs
@@ -5,19 +5,22 @@ namespace LevelObjects.ForceManipulators
 {
     public abstract class AGravityManipulator : MonoBehaviour
     {
-        [Header("Gravity Directions")]
-        [SerializeField] protected Vector3 direction1 = Vector3.zero;
-        [SerializeField] protected Vector3 direction2 = Vector3.zero;
-
         [Header("Gravity Snap Points")]
         [SerializeField] protected Transform snapPoint1;
         [SerializeField] protected Transform snapPoint2;
         
-        private void OnCollisionExit(Collision other)
+        protected virtual void OnTriggerExit(Collider other)
         {
             // snap the gravity of the player to the closest point
             if (other.gameObject.CompareTag("Player"))
             {
+                // if we're not in range of either exit snap point, don't remove the custom gravity
+                if (Vector3.Distance(other.transform.position, snapPoint1.position) > 0.1f &&
+                    Vector3.Distance(other.transform.position, snapPoint2.position) > 0.1f && Vector3.Distance(transform.position, other.transform.position) < 1)
+                {
+                    return;
+                }
+                
                 var mov = other.gameObject.GetComponent<CharacterMovement>();
                 
                 Vector3 newGravityAcceleration = Vector3.Distance(snapPoint1.transform.position, other.transform.position) <
--- Assets/Scripts/LevelObjects/ForceManipulators/GravitySlope.cs
+++ Assets/Scripts/LevelObjects/ForceManipulators/GravitySlope.cs
@@ -7,19 +7,12 @@ namespace LevelObjects.ForceManipulators
         [Header("Slope Settings")] [SerializeField]
         private Transform slopeNormal;
         
-        private void OnCollisionEnter(Collision collision)
+        private void OnTriggerStay(Collider collision)
         {
             if (collision.gameObject.CompareTag("Player"))
             {
-                Debug.Log("Collision Enter");
-                // if player set custom gravity to follow the direction of the slope
-                ContactPoint cont = collision.GetContact(0); // 0th contact's collider is this object
-                
-                // set gravity to use the down direction of the slope's normal transform
                 var mov = collision.gameObject.GetComponent<CharacterMovement>();
-                
                 mov.SetCustomGravity(slopeNormal.transform.up * Physics.gravity.magnitude * -1);
-                // also make sure to adjust the rotation of the player on the necessary axes
             }
         }
     }
