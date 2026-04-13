df8bd29fbdc6d156186695e4b2a005c0ea718aa4
Nora Dwelly <dwelly.412@gmail.com>
2026-04-13 16:43:21 +0100

    refactor: Move collision adjustment to dedicated component for reuse

---
+++ Assets/Scripts/ColliderAdjuster.cs
@@ -0,0 +1,102 @@
+using UnityEngine;
+using GameConstants.Enumerations;
+using GameConstants;
+using System.Collections.Generic;
+using Managers;
+
+
+public class ColliderAdjuster : MonoBehaviour
+{
+    private List<BoxCollider> attachedColliders= new();
+    private List<ColliderSettings> defaultSettings = new();
+    
+    private void Start()
+    {
+       // set up the lists
+       attachedColliders.AddRange(GetComponents<BoxCollider>());
+       // set the default collider settings
+       
+       foreach (var col in attachedColliders)
+       {
+           defaultSettings.Add(new ColliderSettings(col));
+       }
+       
+       // set up the event sub for dim switching
+       PerspectiveSwitcher.OnDimensionsSwitched += DimensionSwitchHandler;
+    }
+
+    private void OnDestroy()
+    {
+        PerspectiveSwitcher.OnDimensionsSwitched -= DimensionSwitchHandler;
+    }
+    
+    protected virtual void DimensionSwitchHandler(Dimensions newDim)
+    {
+        if (newDim == Dimensions.THIRD)
+        {
+            // restore colliders to their default settings
+            for (int i = 0; i < attachedColliders.Count; i++)
+            {
+                defaultSettings[i].CopySettingsToCollider(attachedColliders[i]);
+            }
+        }
+        else
+        {
+            // change the collider settings so that they are centred on the player on the current axis
+            for (int i = 0; i < attachedColliders.Count; i++)
+            {
+                // if it's a box collider set the center bounds
+                Axes axis = PerspectiveSwitcher.CurrentObservedAxis;
+                attachedColliders[i].center = attachedColliders[i].transform.InverseTransformPoint(
+                    SetValueOnAxis(attachedColliders[i].center,
+                        GameController.Singleton.GetPlayerAxisValue(axis), axis)
+                );
+                // HACK: do this to make sure that the button collider's y position isn't changed
+                attachedColliders[i].center = SetValueOnAxis(attachedColliders[i].center, defaultSettings[i].position.y, Axes.Y);
+            }
+        }
+    }
+
+    private Vector3 SetValueOnAxis(Vector3 inp, float newValue, Axes axis)
+    {
+        Vector3 vec = inp;
+            
+        switch (axis)
+        {
+            case Axes.X:
+                vec.x = newValue;
+                break;
+            case Axes.Y:
+                vec.y = newValue;
+                break;
+            case Axes.Z:
+                vec.z = newValue;
+                break;
+        }
+
+        return vec;
+    }
+}
+
+public struct ColliderSettings
+{
+    // struct to hold collider settings
+    public Vector3 position;
+    public Vector3 size;
+    public bool isTrigger;
+
+    public ColliderSettings(BoxCollider target)
+    {
+        // copies specified info from target collider
+        position = target.center;
+        size = target.size;
+        isTrigger = target.isTrigger;
+    }
+    
+    public void CopySettingsToCollider(BoxCollider target)
+    {
+        target.center = position;
+        target.size = size;
+        target.isTrigger = isTrigger;
+    }
+}
