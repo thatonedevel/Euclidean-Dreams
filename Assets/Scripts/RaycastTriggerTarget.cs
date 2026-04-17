2e7db6ef1d85e3a1517d535a7b293736ceeb7c10
Nora Dwelly <dwelly.412@gmail.com>
2026-04-17 14:02:38 +0100

    feat: Add equality comparison based on GUID

--- Assets/Scripts/RaycastTriggerTarget.cs
+++ Assets/Scripts/RaycastTriggerTarget.cs
@@ -1,10 +1,19 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System;
+using System.Linq;
 
 public class RaycastTriggerTarget : MonoBehaviour
 {
+    private List<RaycastCollision> activeCollisions = new();
+    
     private void OnRaycastTriggerEnter(RaycastCollision collision)
     {
-        
+        // check if we have this in our active collisions
+        if (!activeCollisions.Contains(collision))
+        {
+            // new collision occurred
+        }
     }
 
     private void OnRaycastTriggerExit(RaycastCollision collision)
@@ -13,10 +22,19 @@ public class RaycastTriggerTarget : MonoBehaviour
     }
 }
 
-public struct RaycastCollision
+public struct RaycastCollision : IEquatable<RaycastCollision>
 {
     // contains info about a collision from a raycast with extra info
-    public RaycastHit hit;
-    public GameObject gameObject;
+    public RaycastHit hit { get; private set; }
+    public GameObject gameObject { get; private set; }
+    public Guid collisionGuid { get; private set; }
+
+    public RaycastCollision(RaycastHit hit, GameObject source)
+    {
+        this.hit = hit;
+        gameObject = source;
+        collisionGuid = Guid.NewGuid();
+    }
     
+    public bool Equals(RaycastCollision other) => other.collisionGuid == collisionGuid;
 }
