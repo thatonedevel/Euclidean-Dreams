0a0d562e8f29b8968e1fd48b39a6c42b8f660bc4
Nora Dwelly <dwelly.412@gmail.com>
2026-04-25 16:22:56 +0100

    feat: Add methods to move objects back and forth on the rail

--- Assets/Scripts/LevelObjects/OnRailHook.cs
+++ Assets/Scripts/LevelObjects/OnRailHook.cs
@@ -1,18 +1,40 @@
 using UnityEngine;
 
-public class OnRailHook : MonoBehaviour
+namespace LevelObjects
 {
-    // object that moves across a rail. acts as a container for other objects that will be placed onto this as children
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class OnRailHook : MonoBehaviour
     {
-        
-    }
+        // object that moves across a rail. acts as a container for other objects that will be placed onto this as children
+        [SerializeField] private Rail parentRail;
+        [SerializeField] private float moveSpeed;
+        [SerializeField] private float snapThreshold = 0.1f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private float tDelta = 0;
+        private float t = 0;
+    
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            // calculate how much t should change per frame that the move methods are called
+            var length = parentRail.GetRailLength();
+            tDelta = length * moveSpeed;
+        }
+
+        public void MoveForward()
+        {
+            // increase t and update pos
+            t += tDelta * Time.deltaTime;
+            // if t is close to 1, snap it
+            t = t >= 1 - snapThreshold ? 1 : t;
+            transform.position = parentRail.GetPointOnRail(t);
+        }
+
+        private void MoveBackwards()
+        {
+            // decrease t and update pos
+            t -= tDelta * Time.deltaTime;
+            t = t <= snapThreshold? 0 : t;
+            transform.position = parentRail.GetPointOnRail(t);
+        }
     }
 }
