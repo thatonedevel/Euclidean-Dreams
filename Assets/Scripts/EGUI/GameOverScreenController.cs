68fd3c700464f9569ad5228e919c81350f6942f2
Nora Dwelly <dwelly.412@gmail.com>
2026-04-28 14:52:03 +0100

    progress

--- Assets/Scripts/EGUI/GameOverScreenController.cs
+++ Assets/Scripts/EGUI/GameOverScreenController.cs
@@ -13,13 +13,20 @@ namespace EGUI
         private VisualElement root;
         
         // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        private void Start()
         {
             root = gameOverDocument.rootVisualElement.Q<VisualElement>("Root");
             restartButton = root.Q<Button>("RestartButton");
             titleScreenButton = root.Q<Button>("TitleScreenButton");
 
             GameController.OnGameStateChanged += GameUpdateListener;
+            restartButton.clicked += RestartButtonClicked;
+            titleScreenButton.clicked += TitleScreenButtonClicked;
+        }
+
+        private void OnDestroy()
+        {
+            GameController.OnGameStateChanged -= GameUpdateListener;
         }
 
         private void GameUpdateListener(GameStates newState, GameStates oldState)
