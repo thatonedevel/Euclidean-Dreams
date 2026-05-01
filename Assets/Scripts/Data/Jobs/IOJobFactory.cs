d07095a9afd213a551730d54f5651915b0b903b6
Nora Dwelly <dwelly.412@gmail.com>
2026-05-01 15:21:03 +0100

    feat: Factory implementation

--- Assets/Scripts/Data/Jobs/IOJobFactory.cs
+++ Assets/Scripts/Data/Jobs/IOJobFactory.cs
@@ -5,16 +5,23 @@ namespace Data.Jobs
 {
     public static class IOJobFactory
     {
-        public static void CreateJob(string path, IOJobType opType)
+        public static IOJob CreateJob(string path, IOJobType opType, string dataToWrite="")
         {
+            IOJob j;
+            
             if (opType == IOJobType.WRITE)
             {
-                
+                j = new WriteJob();
+                // give the data to the job that we want to write to disk
+                ((WriteJob)j).SetWriteBytes(dataToWrite);
             }
             else
             {
-                
+                j = new ReadJob();
             }
+
+            j.SetPath(path);
+            return j;
         }
     }
 
